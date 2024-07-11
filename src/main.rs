iximeow/yaxgbc

commit 90957efe5350b0fe20ef905e1e6a216c5d63d919

do not test for or run dma transfers in the hot path
proably should factor this out into some kind of utility function, but
the primary purpose of this change is to move dma transfer checking and
execution out of `GBC::run`. in that function, we regularly check for
dma transfers that almost never happen, and grow the stack frame size
(and some of the subsequent initialization).. again, for code that is
very unlikely to run.

this was some (~2-5%?) improvement on windows and linux

src/main.rs
@@ -1295,8 +1295,17 @@ impl MemoryBanks for MemoryMapping<'_> {
             } else if reg == HDMA4 {
                 self.management_bits[reg] = value;
             } else if reg == HDMA5 {
-                self.dma_requested = true;
-                self.management_bits[reg] = value;
+                let source = (self.management_bits[HDMA1] as u16) << 8 | ((self.management_bits[HDMA2] as u16 & 0xf0));
+                let dest = ((((self.management_bits[HDMA3] as u16) << 8) & 0x1fff) | 0x8000) | ((self.management_bits[HDMA4] as u16 & 0xf0));
+                let size = self.management_bits[HDMA5] as u16;
+                if size > 0x7f {
+                    panic!("TODO: hblank dma");
+                }
+                let size = size * 0x10 + 0x10;
+                for i in 0..size {
+                    self.store(dest + i, self.load(source + i));
+                }
+                self.management_bits[reg] = 0;
             } else if reg == SCX {
 //                eprintln!("SCX set to {:02x}", value);
                 self.management_bits[reg] = value;
@@ -1862,18 +1871,6 @@ impl GBC {
                 mem_map.load(self.cpu.pc + 2),
                 mem_map.load(self.cpu.pc + 3),
             );
-        }
-        if mem_map.dma_requested {
-            let source = (mem_map.management_bits[HDMA1] as u16) << 8 | ((mem_map.management_bits[HDMA2] as u16 & 0xf0));
-            let dest = ((((mem_map.management_bits[HDMA3] as u16) << 8) & 0x1fff) | 0x8000) | ((mem_map.management_bits[HDMA4] as u16 & 0xf0));
-            let size = mem_map.management_bits[HDMA5] as u16;
-            if size > 0x7f {
-                panic!("TODO: hblank dma");
-            }
-            let size = size * 0x10 + 0x10;
-            for i in 0..size {
-                mem_map.store(dest + i, mem_map.load(source + i));
-            }
         }
         if self.cpu.sp >= 0xfe00 && self.cpu.sp < 0xff80 {
             panic!("nonsense sp: ${:04x}", self.cpu.sp);

src/cpu.rs
@@ -350,6 +350,7 @@ impl<T: yaxpeax_arch::Reader<<SM83 as Arch>::Address, <SM83 as Arch>::Word>> yax
         self.cpu.af[1] = self.storage.load(0xff00 + self.cpu.bc[0] as u16);
         Ok(())
     }
+    #[inline(always)]
     fn on_add_16b_hl_rr(&mut self, op: Reg16b) -> Result<(), <SM83 as Arch>::DecodeError> {
         let hl = u16::from_le_bytes(self.cpu.hl);
         let other = match op {
@@ -389,6 +390,7 @@ impl<T: yaxpeax_arch::Reader<<SM83 as Arch>::Address, <SM83 as Arch>::Word>> yax
         }
         Ok(())
     }
+    #[inline(always)]
     fn on_inc_16b_rr(&mut self, op0: Reg16b) -> Result<(), <SM83 as Arch>::DecodeError> {
         match op0 {
             Reg16b::BC => { self.cpu.bc = u16::from_le_bytes(self.cpu.bc).wrapping_add(1).to_le_bytes(); },

commit 35f72126aa732fcb3f2c067e162376c635945113

respect hdma transfer size
fixes bug introduced in 90957efe5350b0fe20ef905e1e6a216c5d63d919

src/main.rs
@@ -1537,6 +1537,7 @@ impl MemoryBanks for MemoryMapping<'_> {
             } else if reg == HDMA4 {
                 self.state.management_bits[reg] = value;
             } else if reg == HDMA5 {
+                self.state.management_bits[HDMA5] = value;
                 let source = (self.state.management_bits[HDMA1] as u16) << 8 | ((self.state.management_bits[HDMA2] as u16 & 0xf0));
                 let dest = ((((self.state.management_bits[HDMA3] as u16) << 8) & 0x1fff) | 0x8000) | ((self.state.management_bits[HDMA4] as u16 & 0xf0));
                 let size = self.state.management_bits[HDMA5] as u16;